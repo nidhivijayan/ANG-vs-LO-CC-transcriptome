f88123109bef5377c3e900871c53d5e9fa66d994
nidhivijayan <80131639+nidhivijayan@users.noreply.github.com>
2024-02-25 16:52:32 -0500

    Create abundance_estimates_to_matrix.sh

---
+++ abundance_estimates_to_matrix.sh
@@ -0,0 +1,10 @@
+module load trinity/2.8.5
+module load kallisto/0.44.0
+module load R/3.6.1
+Rscript -e "library(edgeR)"
+
+/isg/shared/apps/trinity/2.8.5/util/abundance_estimates_to_matrix.pl --est_method kallisto \
+	--gene_trans_map triniity.fasta.gene_trans_map \
+	--out_prefix kallisto \
+	--name_sample_by_basedir \
+#  *output of kallisto/abundance.tsv of all samples
