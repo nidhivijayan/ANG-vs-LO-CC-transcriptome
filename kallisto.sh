d90651eb2d15630ae4967ffb1e5807be05b42bf6
nidhivijayan <80131639+nidhivijayan@users.noreply.github.com>
2024-02-25 16:50:26 -0500

    Create kallisto.sh

---
+++ kallisto.sh
@@ -0,0 +1,15 @@
+module load kallisto/0.44.0
+
+#First index the fasta file
+kallisto index -i trinity.fasta.index Trinity.fasta
+
+#Then quantify with kallisto
+kallisto quant -i trinity.fasta.index -o kallisto_quant_CCQ3 -t 8 ${input}/167753_CCQ3_FP.fastq.gz ${input}/167753_CCQ3_RP.fastq.gz 
+
+for FILE in ${input}
+	do
+#		#This sets prefixes for in/outs downstream# 
+	prefix=`echo ${FILE} | cut -d "/" -f 9 | cut -d "." -f 1`
+	
+		kallisto quant -i centroids.fasta.index -o kallisto_quant_${prefix} -t 8 ${FILE}/${prefix}_FP.fq.gz ${FILE}/${prefix}_RP.fq.gz 
+done
